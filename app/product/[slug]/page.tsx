0e3b8a6df2a5b3bed1b0d92e3b6a992fd4efc09e
Zekima <christianledaal@gmail.com>
2024-03-08 19:35:11 +0100

    fix: manual image optimization and fallback

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -7,17 +7,17 @@ export default function Page({ params }: { params: { slug: string } }) {
     const products = [
         {
             title: "Windows 11 Pro Retail",
-            image: "/winpro11.avif",
+            image: "/winpro11",
             price: 349
         },
         {
             title: "Windows 11 Home Retail",
-            image: "/winhome11.avif",
+            image: "/winhome11",
             price: 275
         },
         {
             title: "Office 2021 Professional Plus",
-            image: "/off2021.avif",
+            image: "/off2021",
             price: 450
         }
     ]
@@ -34,13 +34,13 @@ export default function Page({ params }: { params: { slug: string } }) {
             <div className="flex gap-2 sm:flex-row flex-col">
                 <div>
 
+
                     <div className="bg-white ml-0 mr-3 my-6 relative h-[320px] w-[300px] select-none">
-                        <Image
-                            src={product.image}
-                            priority
-                            fill
-                            alt={product.title}
-                        />
+                        <picture>
+                            <source srcSet={`${product.image}.avif`} type="image/avif" />
+                            <source srcSet={`${product.image}.webp`} type="image/webp" />
+                            <img alt={product.title} />
+                        </picture>
                     </div>
                     <h3></h3>
                 </div>
@@ -50,7 +50,7 @@ export default function Page({ params }: { params: { slug: string } }) {
                     <input type="text" className="p-3 border border-black my-3 rounded-md" id="emailInput" />
                     <button className="bg-blue-600 py-4 px-10 text-white font-medium rounded-md">Kjøp Nå</button>
                 </div>
-                
+
             </div>
 
 
--- components/card-container.tsx
+++ components/card-container.tsx
@@ -7,17 +7,17 @@ const CardContainer = () => {
     const products = [
         {
             title: "Windows 11 Pro Retail",
-            image: "/winpro11.avif",
+            image: "/winpro11",
             price: 349
         },
         {
             title: "Windows 11 Home Retail",
-            image: "/winhome11.avif",
+            image: "/winhome11",
             price: 275
         },
         {
             title: "Office 2021 Professional Plus",
-            image: "/off2021.avif",
+            image: "/off2021",
             price: 450
         }
     ]
@@ -29,20 +29,17 @@ const CardContainer = () => {
             <div className="grid lg:grid-cols-3 grid-cols-1 gap-3 mt-[100px]">
                 {products.map((product) => (
                     <Link key={product.title} href={`/product/${product.title}`}>
-                    <div  className="border p-4 rounded-md bg-white transistion hover:scale-105 duration-150 cursor-pointer">
-                        <div className="w-[200px] h-[200px] p-4 justify-center flex items-center relative">
-
-                            <Image
-                                src={product.image}
-                                height={200}
-                                width={200}
-                                priority
-                                alt={product.title}
-                            />
+                        <div className="border p-4 rounded-md bg-white transistion hover:scale-105 duration-150 cursor-pointer">
+                            <div className="w-[200px] h-[200px] p-4 justify-center flex items-center relative">
+                                <picture>
+                                    <source srcSet={`${product.image}.avif`} type="image/avif" />
+                                    <source srcSet={`${product.image}.webp`} type="image/webp" />
+                                    <img alt={product.title} />
+                                </picture>
+                            </div>
+                            <p className="">{product.title}</p>
+                            <p className="text-xl text-black font-bold">{product.price} kr</p>
                         </div>
-                        <p className="">{product.title}</p>
-                        <p className="text-xl text-black font-bold">{product.price} kr</p>
-                    </div>
                     </Link>
                 ))}
 
