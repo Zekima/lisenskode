46b2f263da0e23147d83ad568ae562180a0712f9
Zekima <christianledaal@gmail.com>
2024-03-08 12:28:52 +0100

    fix: compile error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-import FooterLinks from '@/components/footer-links.tsx'
+import FooterLinks from '@/components/footer-links'
 
 export const metadata: Metadata = {
   title: "LisensKode",
